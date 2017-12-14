9926c3d3df81e4f60c4a5665b9a9c9323df857ca
teresita <t.canette@gmail.com>
2017-12-14 11:30:57 -0300

    subiendo lyft

---
+++ assent/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    setTimeout(function() {
+        $('.load').fadeOut(500);
+      },2000);
+          setTimeout(function() {
+               $('.container').fadeIn(500); // <-- time in milliseconds
+
+    });
+});
+
